6252ac8f49351c32f0060678262cfabd740313a6
diwakar4586 <diwakar0470.be21@chitkara.edu.in>
2022-03-15 18:09:36 +0530

    added multiply.cpp

---
+++ multiply.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int a;
+    cin>>a;
+    int b;
+    cin>>b;
+    int multiply = a*b;;
+    cout<<"product is"<<"::"<<multiply;
+    return 0;
+    return 0;
+}
