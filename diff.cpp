6454f05cd85b38a6742b87ccbd9294a8e62bfac0
diwakar4586 <diwakar0470.be21@chitkara.edu.in>
2022-03-15 17:54:03 +0530

    adding diff.cpp

---
+++ diff.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int a;
+    cin>>a;
+    int b;
+    cin>>b;
+    int Diff = a-b;
+    cout<<"Difference is"<<"::"<<Diff;
+    return 0;
+    return 0;
+}
