07cc1c28c145547374ab0f8a226d196e72aa1f54
DEVANSH-DOGRA <devansh0422.be21@chitkara.edu.in>
2022-05-29 15:18:31 +0530

    appended a code that takes 5 array elements and prints their sum

--- multiply.cpp
+++ multiply.cpp
@@ -7,7 +7,17 @@ int main(){
     int b;
     cin>>b;
     int multiply = a*b;;
-    cout<<"product is"<<"::"<<multiply;
+    cout<<"product is"<<"::"<<multiply<<endl;
+    // I AM DEVANSH.I AM APPENDING A CODE THAT TAKES 5 ARRAY ELEMENTS AND PRINTS THEIR SUM
+         cout << "Enter the 5 array elements:" << endl;
+    int arr[5];
+    int sum=0;
+    for(int i=0;i<5;i++)
+    {
+        cin>>arr[i];
+        sum=sum+arr[i];
+    }
+    cout<<"The sum of array elements is: "<<sum<<endl;
     return 0;
-    return 0;
+
 }
